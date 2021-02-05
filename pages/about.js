19d58cb28bdc48e6b9fcbe16035ef1cb4eebc267
Ricardo Lamadrid <ricardo.lamadrid@bldr.com>
2021-02-05 17:02:52 -0600

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet repellat, cum eius reprehenderit quam earum perspiciatis harum illum sint ipsa. Sapiente aspernatur tenetur delectus dolorem exercitationem unde, maxime temporibus id!</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet repellat, cum eius reprehenderit quam earum perspiciatis harum illum sint ipsa. Sapiente aspernatur tenetur delectus dolorem exercitationem unde, maxime temporibus id!</p>
+      </div>
+    </>
+  );
+}
+ 
+export default About;
