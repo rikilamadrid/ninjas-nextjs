21eb53cd0b7b871f9fbafd9180c8007a891c5628
Ricardo Lamadrid <ricardo.lamadrid@bldr.com>
2022-12-10 11:07:02 -0600

    update text

--- pages/about.js
+++ pages/about.js
@@ -9,8 +9,8 @@ const About = () => {
       </Head>
       <div>
         <h1>About</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet repellat, cum eius reprehenderit quam earum perspiciatis harum illum sint ipsa. Sapiente aspernatur tenetur delectus dolorem exercitationem unde, maxime temporibus id!</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet repellat, cum eius reprehenderit quam earum perspiciatis harum illum sint ipsa. Sapiente aspernatur tenetur delectus dolorem exercitationem unde, maxime temporibus id!</p>
+        <p>The best places to appreciate the ninja today are Iga and Koka, the traditional heartlands of the ninja, and Togakushi in Nagano, home of the Togakure school of ninja. These respective regions offer some actual buildings used by the ninja in the past and/or museums which house previously used tools and weapons, thus retaining some level of authenticity.</p>
+        <p>The ninja were stealth soldiers and mercenaries hired mostly by daimyōs. Their primary roles were those of espionage and sabotage, although assassinations were also attributed to ninja. Although they were considered the anti-samurai and were disdained by those belonging to the samurai class, they were necessary for warfare and were even employed by the samurai themselves to carry out operations that were forbidden by bushidō.</p>
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,12 @@ export default function Home() {
       <Head>
         <title>Ninja List | Home</title>
         <meta name="keywords" content="ninjas"/>
+        <link rel="icon" href="/ninja-favicon.png" />
       </Head>
       <div>
         <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quaerat, alias exercitationem, maxime distinctio voluptates reprehenderit nemo aut corporis commodi id nam dolorum quod vero iste ducimus eos animi molestiae?</p>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quaerat, alias exercitationem, maxime distinctio voluptates reprehenderit nemo aut corporis commodi id nam dolorum quod vero iste ducimus eos animi molestiae?</p>
+        <p className={styles.text}>The diverse range of skills practiced by the ninja is called ninjutsu - the art of stealth - and includes training in armed combat, weapon techniques, military strategy, meteorology, geometry and breathing synthesis. Ninjutsu was passed down through generations within families or directly from teacher to a selected one or few disciples. This secrecy in part explains why little factual information exists about the ninja, giving rise to lots of stereotyping of them both within and outside of Japan.</p>
+        <p className={styles.text}>As early as the 11th century, the inhabitants of Iga and Koka gained a reputation for their skills in unconventional warfare. Close to the political center Kyoto, yet distinctly remote due to their rugged terrains, Iga and Koka presented an ideal hideout location for refugees, bandits and losing parties of battles. Many ninja families lived here within autonomously governed communities, developing their martial skills for self defense.</p>
         <Link href="/ninjas">
           <a className={styles.btn}>See Ninja Listing</a>
         </Link>
